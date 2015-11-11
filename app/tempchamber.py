f93e3b531c898dad003196f42ffc05a0193ad70e
Christopher Frederickson <chris.foody@gmail.com>
2015-11-11 14:39:25 -0500

    Add tempchamber module

---
+++ app/tempchamber.py
@@ -0,0 +1,19 @@
+import minimalmodbus
+import serial
+
+class TempChamber:
+       
+    def __init__(self):
+        self.instrument = minimalmodbus.Instrument('COM6', 1)
+        self.instrument.serial.baudrate = 19200
+        self.instrument.serial.bytesize = 8
+        self.instrument.serial.parity   = serial.PARITY_NONE
+        self.instrument.serial.stopbits = 1
+        self.instrument.serial.timeout  = 0.05
+        
+    def set_temp(self, temp):
+        formatted_temp = int(temp*10)
+        self.instrument.write_register(300, formatted_temp, 0, 16, False)
+    
+    def read_temp(self):
+        return self.instrument.read_register(100, 1, 3, False)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,28 @@
-from app import app
+from app import app, tempchamber
+from flask import request
+
+chamber = tempchamber.TempChamber()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/read')
+def read_temp():
+    try:
+        temp = chamber.read_temp()
+    except:
+        return 'Fail! Temp read error.'
+    return str(temp)
+
+@app.route('/set')
+def set_temp():
+    try:
+        temp = float(request.args.get('temp'))
+    except:
+        return 'Fail! Invalid argument'
+    try:
+        chamber.set_temp(temp)
+    except:
+        return 'Fail! Temp set error'
+    return 'Success!'
