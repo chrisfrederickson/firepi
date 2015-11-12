74d8390dce4dd5a8fe8a6f7f4304b80afadfec1d
Christopher Frederickson <chris.foody@gmail.com>
2015-11-12 11:07:23 -0500

    Add basic calendar framework

---
+++ app/calender.py
@@ -0,0 +1,16 @@
+class CalenderManager:
+    def __init__(self):
+        pass
+    
+    def add_event(self, user, starttime, endtime):
+        pass
+    
+    def get_current_event(self):
+        #Return NONE if no event
+        pass
+    
+    def remove_event(self, id):
+        pass
+    
+    def get_upcoming_events(self, num):
+        pass
